fb99674b22ef98d330c921bf1bfcb969304fd422
Pradeep L <pradeepcodes789@gmail.com>
2025-05-02 16:21:16 +0530

    Add data_ingestion/weather_events.py 1746183075712

--- data_ingestion/weather_events.py
+++ data_ingestion/weather_events.py
@@ -13,7 +13,7 @@ class WeatherEventsIngestor:
         event_types = ["concert", "sports_game", "festival", "none"]
         data = {}
         base_date = datetime.strptime("2020-01-01", "%Y-%m-%d")
-        for i in range(100):
+        for i in range(1000):
             date = (base_date + timedelta(days=i)).strftime("%Y-%m-%d")
             weather = random.choice(weather_conditions)
             event = random.choice(event_types)
